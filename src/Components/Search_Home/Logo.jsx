528b46fabcc3b4114e5faa8eecc8be6b5d1d9675
louremipsum <myself.vinayak2@gmail.com>
2022-11-22 19:54:49 +0530

    feat: added Logo component

---
+++ src/Components/Search_Home/Logo.jsx
@@ -0,0 +1,10 @@
+import { Text } from '@mantine/core'
+import React from 'react'
+
+function Logo() {
+  return (
+    <Text fw={700} size="32px" color={"white"} m="lg">Ingri-to-Food</Text>
+  )
+}
+
+export default Logo
