972d2576e2ada6a2ed57ed821004841163c2fb93
louremipsum <myself.vinayak2@gmail.com>
2022-11-23 22:11:45 +0530

    try: implement scrollInView

--- src/Components/Search_Home/Bg.jsx
+++ src/Components/Search_Home/Bg.jsx
@@ -1,35 +1,53 @@
-import { BackgroundImage, createStyles, Stack,Text } from '@mantine/core'
-import React from 'react'
+import {
+  BackgroundImage,
+  createStyles,
+  Stack,
+  Text,
+  Button,
+} from "@mantine/core";
+import { useScrollIntoView } from "@mantine/hooks";
+import React from "react";
 // import {ReactComponent as Background} from '../assets/layered-waves-haikei.svg'
-import L from '../assets/layered-waves-haikei.png'
-import Logo from './Logo'
-import Search from './Search'
-
-
+import L from "../assets/layered-waves-haikei.png";
+import Receipe from "../Receipi/Receipe";
+import Logo from "./Logo";
+import Search from "./Search";
 
 function Bg() {
- const {classes} = useStyles();
+  const { classes } = useStyles();
+  const { scrollIntoView, targetRef } = useScrollIntoView({ offset: 60 });
   return (
     <>
-    <BackgroundImage src={L} className={classes.main}>
+      <BackgroundImage src={L} className={classes.main}>
         <Stack align={"center"}>
-            <Logo/>
-            <Text p={"md"} fw="400" size={"md"} c="dimmed" italic>Search from the Left Box and check all the ingridients which you have and press the cart button to proceed</Text>
-            <Search/>
+          <Logo />
+          <Text p={"md"} fw="400" size={"md"} c="dimmed" italic>
+            Search from the Left Box and check all the ingridients which you
+            have and press the cart button to proceed
+          </Text>
+          <Search />
+          <Button
+            color={"blue"}
+            size="md"
+            onClick={() => scrollIntoView({ alignment: "center" })}
+          >
+            Get Receipes
+          </Button>
         </Stack>
-    </BackgroundImage >
-    {/* <Background className={classes.main}/> */}
+      </BackgroundImage>
+      <Receipe {...targetRef} />
+      {/* <Background className={classes.main}/> */}
     </>
-  )
+  );
 }
 
-const useStyles = createStyles(()=>({
-   main:{  
-   backgroundPosition: "center",
-   backgroundRepeat: "no-repeat",
-   backgroundSize: "cover",
-   height: "100vh"
-   }
+const useStyles = createStyles(() => ({
+  main: {
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100vh",
+  },
 }));
 
-export default Bg
+export default Bg;
